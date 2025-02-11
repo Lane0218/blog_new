921d1d2ffa603b76b112d2f179ddc296dcd87138
Lane0218 <laneljc@qq.com>
2025-02-11 23:41:11 +0800

    新增计网和算法设计与分析实验内容+lab模板

--- docs/.vuepress/notes/KB.ts
+++ docs/.vuepress/notes/KB.ts
@@ -8,22 +8,32 @@ export default defineNoteConfig({
         {
             text: '数据结构与算法',
             prefix: '1.algorithms',
-            items: "auto"
+            items: "auto",
+            collapsed: true
         },
         {
             text: '数字逻辑与计算机体系结构',
             prefix: '2.DDCA',
-            items: "auto"
+            items: "auto",
+            collapsed: true
         },
         {
             text: '计算机系统',
             prefix: '3.CSAPP',
-            items: "auto"
+            items: "auto",
+            collapsed: true
         },
         {
             text: '计算机网络',
             prefix: '4.network',
-            items: "auto"
+            items: "auto",
+            collapsed: true
+        },
+        {
+            text: '算法设计与分析',
+            prefix: '5.algorithms_2',
+            items: "auto",
+            collapsed: true
         },
     ]
 })
